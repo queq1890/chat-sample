e786486e2a1e12f61ee990d94e408b17855a1663
queq1890 <yujimatsumoto@matsumotohiroshinoMacBook-Pro.local>
2017-10-03 19:21:28 +0900

    Create registrations controller to update without password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,17 @@
 class UsersController < ApplicationController
 
   def edit
+    @user = current_user
   end
 
   def update
+   user = current_user
+   user.update(user_params)
+  end
+
+  private
+
+  def user_params
+    params(:user).permit(:name, :email)
   end
 end

ee4378755671d5f2d8bd1543bd04e6e21f2514cf
queq1890 <yujimatsumoto@matsumotohiroshinoMacBook-Pro.local>
2017-10-04 15:23:22 +0900

    define after_sign_out_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
 
+  def after_sign_out_path_for(resources)
+    '/users/sign_in'
+  end
+
   private
 
   def configure_permitted_parameters

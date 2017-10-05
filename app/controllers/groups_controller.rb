21063924d5f27c5e533da36283b5d5ed4138ce35
queq1890 <yujimatsumoto@matsumotohiroshinoMacBook-Pro.local>
2017-10-05 14:59:44 +0900

    Add variable numbers to create group instance

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,8 +4,18 @@ class GroupsController < ApplicationController
   end
 
   def new
+    @group = Group.new
+  end
+
+  def create
+    Group.create(create_params)
   end
 
   def edit
   end
+
+  private
+    def create_params
+      params.permit(user_ids: [],name: "")
+    end
 end

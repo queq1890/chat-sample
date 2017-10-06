3064a9b2231c68e7b48be2eae2ab5a20f370215d
queq1890 <yujimatsumoto@matsumotohiroshinoMacBook-Pro.local>
2017-10-06 13:06:38 +0900

    Set group_params, define update action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -9,20 +9,31 @@ class GroupsController < ApplicationController
   end
 
   def create
-    @group = Group.new(create_params)
+    @group = Group.new(group_params)
       if @group.save
         flash[:notice] = "グループを作成しました"
         redirect_to root_path
       else
-        render'new'
+        render 'new'
       end
   end
 
   def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    group = Group.find(params[:id])
+      if group.update(group_params)
+        flash[:notice] = "グループを編集しました"
+        redirect_to root_path
+      else
+        render 'edit'
+      end
   end
 
   private
-    def create_params
+    def group_params
       params.require(:group).permit(:name, user_ids: [])
     end
 end

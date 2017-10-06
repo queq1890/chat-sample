cee0b51c292aafe091f931e3a7f5229e2af4eab0
queq1890 <yujimatsumoto@matsumotohiroshinoMacBook-Pro.local>
2017-10-06 15:13:43 +0900

    Change root to groups#index, limit resources actions of groups

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
-  resources :groups
+  resources :groups, except: [:show, :destroy]
   resources :messages
-  root 'messages#index'
+  root 'groups#index'
 end

22aa4398c421dce6126ec28eee0e426404ae865d
queq1890 <yujimatsumoto@matsumotohiroshinoMacBook-Pro.local>
2017-10-06 13:29:05 +0900

    Set set_group and to refactor code

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,5 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit, :update]
 
   def index
   end
@@ -19,12 +20,10 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @group = Group.find(params[:id])
   end
 
   def update
-    group = Group.find(params[:id])
-      if group.update(group_params)
+      if @group.update(group_params)
         flash[:notice] = "グループを編集しました"
         redirect_to root_path
       else
@@ -35,5 +34,9 @@ class GroupsController < ApplicationController
   private
     def group_params
       params.require(:group).permit(:name, user_ids: [])
+    end
+
+    def set_group
+      @group = Group.find(params[:id])
     end
 end

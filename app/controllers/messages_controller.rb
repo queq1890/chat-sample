651d17906bb8743bc40e602570f2482737e51f7a
queq1890 <yujimatsumoto@matsumotohiroshinoMacBook-Pro.local>
2017-10-06 17:12:57 +0900

    Def create action

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,5 +2,21 @@ class MessagesController < ApplicationController
   before_action :authenticate_user!, only: :index
 
   def index
+    @messages = Message.all
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(message_params)
+
+    respond_to do |format|
+      if @message.save
+        format.html { redirect_to  group_messages_path}
+        format.json { render :show, status: :created, location: @message }
+      else
+        format.html { render :new }
+        format.json { render json: @message.errors, status: :unprocessable_entity }
+      end
+    end
   end
 end

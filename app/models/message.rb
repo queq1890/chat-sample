7ff829e2439ce9ac475549bbc67d0e0d40e9ef80
queq1890 <yujimatsumoto@matsumotohiroshinoMacBook-Pro.local>
2017-10-07 16:01:50 +0900

    Modify validation

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,6 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :group
   mount_uploader :image, ImageUploader
-  validates_presence_of :body, :unless => :image?
-  validates_presence_of :image, :unless => :body?
+  validates_presence_of :body, unless: :image?
+  validates_presence_of :image, unless: :body?
 end

1114462e3260c88c93ae823acf88fd83bac75f31
queq1890 <yujimatsumoto@matsumotohiroshinoMacBook-Pro.local>
2017-10-07 13:47:34 +0900

    Make it available to save message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,19 +4,19 @@ class MessagesController < ApplicationController
   def index
     @messages = Message.all
     @message = Message.new
+    @group = Group.find(params[:group_id])
   end
 
   def create
     @message = Message.new(message_params)
-
-    respond_to do |format|
-      if @message.save
-        format.html { redirect_to  group_messages_path}
-        format.json { render :show, status: :created, location: @message }
-      else
-        format.html { render :new }
-        format.json { render json: @message.errors, status: :unprocessable_entity }
-      end
-    end
+    binding.pry
+    @message.save
+    redirect_to group_messages_path
   end
+
+
+  private
+    def message_params
+      params.require(:message).permit(:body).merge(user_id: current_user.id, group_id: params[:group_id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
   resources :groups, except: [:show, :destroy] do
-    resources :messages, except: :show
+    resources :messages
   end
   root 'groups#index'
 end

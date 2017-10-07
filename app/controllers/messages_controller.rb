2428e3304118ebf59e49d0bd8e1ebccde4256bce
queq1890 <yujimatsumoto@matsumotohiroshinoMacBook-Pro.local>
2017-10-07 15:17:48 +0900

    Make it available to upload image through Carrerwave

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,7 +9,6 @@ class MessagesController < ApplicationController
 
   def create
     @message = Message.new(message_params)
-    binding.pry
     @message.save
     redirect_to group_messages_path
   end
@@ -17,6 +16,6 @@ class MessagesController < ApplicationController
 
   private
     def message_params
-      params.require(:message).permit(:body).merge(user_id: current_user.id, group_id: params[:group_id])
+      params.require(:message).permit(:body, :image).merge(user_id: current_user.id, group_id: params[:group_id])
     end
 end

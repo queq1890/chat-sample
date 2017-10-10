36e9897773ed34027ee24bb0c5d9a97a03c2f359
queq1890 <yujimatsumoto@matsumotohiroshinoMacBook-Pro.local>
2017-10-10 18:31:26 +0900

    define an example "is valid with a body"

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -3,6 +3,10 @@ require 'rails_helper'
 describe Message do
   describe '#create' do
     it "is valid with a body" do
+      user = create(:user)
+      group = create(:group)
+      message = build(:message, user_id: user.id, group_id: group.id)
+      expect(message).to be_valid
     end
 
     it "is valid with an image" do

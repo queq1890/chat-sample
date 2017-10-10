a5082b26f64c4b8a4f5baf369c6c80dc93b4d9a1
queq1890 <yujimatsumoto@matsumotohiroshinoMacBook-Pro.local>
2017-10-10 16:44:36 +0900

    Define 6 examples for Message model test

---
+++ spec/models/message_spec.rb
@@ -0,0 +1,22 @@
+require 'rails-helper'
+describe Message do
+  describe '#create' do
+    it "is valid with a body" do
+    end
+
+    it "is valid with an image" do
+    end
+
+    it "is valid with an image and a body" do
+    end
+
+    it "is invalid without a body" do
+    end
+
+    it "is invalid without a group_id" do
+    end
+
+    it "is invalid without a user_id" do
+    end
+  end
+end

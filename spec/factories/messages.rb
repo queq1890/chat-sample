a4ee346800b84a905387435623d98fff4fd87c00
queq1890 <yujimatsumoto@matsumotohiroshinoMacBook-Pro.local>
2017-10-10 18:27:35 +0900

    Modify messages.rb for FactoryGirl

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,8 +1,6 @@
 FactoryGirl.define do
   factory :message do
     body "テスト用メッセージ"
-    user_id "1"
-    group_id "1"
-    image { Rack::Test::UploadedFile.new(Rails.root.join( 'spec/fixtures/jpeg')
+    image { Rack::Test::UploadedFile.new(Rails.root.join('spec', 'fixtures', 'test.jpeg'), 'spec/fixtures/jpeg') }
   end
 end

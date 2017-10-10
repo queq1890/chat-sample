d7f5b1ea065f89fe808b5ac3f707ff33fe02d03c
queq1890 <yujimatsumoto@matsumotohiroshinoMacBook-Pro.local>
2017-10-10 17:34:52 +0900

    Modify how to set image

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -3,6 +3,6 @@ FactoryGirl.define do
     body "テスト用メッセージ"
     user_id "1"
     group_id "1"
-    image { Rack::Test::UploadedFile.new(Rails.root.join('spec', 'support', 'brands', 'logos', 'logo_image.jpg'), 'image/jpeg') }
+    image { Rack::Test::UploadedFile.new(Rails.root.join( 'spec/fixtures/jpeg')
   end
 end

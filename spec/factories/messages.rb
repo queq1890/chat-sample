b063effe13c0c80ccb0ce9dfc53460f21f799a68
queq1890 <yujimatsumoto@matsumotohiroshinoMacBook-Pro.local>
2017-10-11 12:48:04 +0900

    Set association ( factories/message.rb)
    Messageのダミーを作った時に関連するuserとgroupも自動で作るように設定

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -2,5 +2,7 @@ FactoryGirl.define do
   factory :message do
     body Faker::ChuckNorris.fact
     image { Rack::Test::UploadedFile.new(Rails.root.join('spec', 'fixtures', 'test.jpeg'), 'spec/fixtures/jpeg') }
+    user
+    group
   end
 end

82b618f3eb3c2eebb35d21359376cdd051e17e8a
queq1890 <yujimatsumoto@matsumotohiroshinoMacBook-Pro.local>
2017-10-11 12:50:00 +0900

    Delete unnecessary definitions of (user, group)s
    アソシエーションの定義により不要になったexample中のuser,groupの記述を削除

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,48 +1,40 @@
 require 'rails_helper'
+require 'pry-rails'
 
 describe Message do
   describe '#create' do
+
     it "is valid with a body" do
-      user = create(:user)
-      group = create(:group)
-      message = build(:message, image: nil, user_id: user.id, group_id: group.id)
+      message = build(:message, image: nil)
+      binding.pry
       expect(message).to be_valid
     end
 
     it "is valid with an image" do
-      user = create(:user)
-      group = create(:group)
-      message = build(:message, body: nil, user_id: user.id, group_id: group.id)
+      message = build(:message, body: nil)
+      binding.pry
       expect(message).to be_valid
     end
 
     it "is valid with an image and a body" do
-      user = create(:user)
-      group = create(:group)
-      message = build(:message, user_id: user.id, group_id: group.id)
+      message = build(:message)
       expect(message).to be_valid
     end
 
     it "is invalid without an image and a body" do
-      user = create(:user)
-      group = create(:group)
-      message = build(:message, image: nil, body: nil, user_id: user.id, group_id: group.id)
+      message = build(:message, image: nil, body: nil)
       message.valid?
       expect(message.errors[:image]).to include("を入力してください")
     end
 
     it "is invalid without a group_id" do
-      user = create(:user)
-      group = create(:group)
-      message = build(:message, user_id: user.id, group_id: nil)
+      message = build(:message, group_id: nil)
       message.valid?
       expect(message.errors[:group]).to include("を入力してください")
     end
 
     it "is invalid without a user_id" do
-      user = create(:user)
-      group = create(:group)
-      message = build(:message, user_id: nil, group_id: group.id)
+      message = build(:message, user_id: nil)
       message.valid?
       expect(message.errors[:user]).to include("を入力してください")
     end

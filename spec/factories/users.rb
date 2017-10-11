e0bde9d1ba4e2f3941d9b4307f0bc2e88b033274
queq1890 <yujimatsumoto@matsumotohiroshinoMacBook-Pro.local>
2017-10-11 15:59:19 +0900

    Set has_many: :has_many relation

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,5 +6,9 @@ FactoryGirl.define do
     email                 Faker::Internet.email
     password              pass
     password_confirmation pass
+
+    after(:create) do |user|
+      user.groups << create(:group)
+    end
   end
 end

a564f261faa51526f809b447703c96fab5f5d05c
queq1890 <yujimatsumoto@matsumotohiroshinoMacBook-Pro.local>
2017-10-11 15:34:28 +0900

    Add login_user

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -4,6 +4,9 @@ describe MessagesController, type: :controller do
   let(:user) { create(:user) }
 
   describe 'GET #index' do
+    before do
+      login_user user
+    end
   end
 
   describe 'POST #create' do

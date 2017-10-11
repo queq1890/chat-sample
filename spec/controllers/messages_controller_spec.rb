8ca245013558d2557a68f28870c90ba57d79f34d
queq1890 <yujimatsumoto@matsumotohiroshinoMacBook-Pro.local>
2017-10-11 16:00:52 +0900

    Set conditions if a dummy user data signs in or not

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -4,11 +4,24 @@ describe MessagesController, type: :controller do
   let(:user) { create(:user) }
 
   describe 'GET #index' do
-    before do
-      login_user user
+    context 'when user does sign_in' do
+      before do
+        login_user user
+      end
+    end
+
+    context 'when user does not sign_in' do
     end
   end
 
   describe 'POST #create' do
+    context 'when user does sign_in' do
+      before do
+        login_user user
+      end
+    end
+
+    context 'when user does not sign_in' do
+    end
   end
 end

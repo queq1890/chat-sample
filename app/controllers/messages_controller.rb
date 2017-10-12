411c3582a8a294f95d681675d8126da0187356d7
queq1890 <yujimatsumoto@matsumotohiroshinoMacBook-Pro.local>
2017-10-12 17:22:16 +0900

    Add authenticate_user! for create action

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,5 @@
 class MessagesController < ApplicationController
-  before_action :authenticate_user!, only: :index
+  before_action :authenticate_user!, only: [:index, :create]
   before_action :set_messages_group, only: [:index, :create]
 
   def index

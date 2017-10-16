edf99fbe0f74c6c974c095299e3349fdfc409e9b
queq1890 <yujimatsumoto@matsumotohiroshinoMacBook-Pro.local>
2017-10-16 16:14:38 +0900

    Make it available to use incremental search

---
+++ app/assets/javascripts/search.js
@@ -0,0 +1,41 @@
+$(function(){
+  var search_list =$('#user-search-result');
+function appendUser(user) {
+  var html = `<div class="chat-group-user clearfix">
+                <p class="chat-group-user__name">${user.name}</p>
+                <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
+              </div>`
+    search_list.append(html);
+ }
+
+function appendNouser(user) {
+    var html = `<div class="chat-group-user clearfix">
+                <p class="chat-group-user__name">${user}</p>
+              </div>`
+    search_list.append(html);
+  }
+
+  $("#user-search-field").on("keyup", function(){
+    var input = $("#user-search-field").val();
+    $.ajax({
+    type: 'GET',
+    url: '/users',
+    data: { keyword: input },
+    dataType: 'json'
+  })
+  .done(function(users) {
+    search_list.empty();
+    if (users.length !== 0) {
+      users.forEach(function(user){
+        appendUser(user);
+      });
+    }
+    else {
+      appendNouser("一致するユーザーが見つかりませんでした");
+    }
+  })
+  .fail(function() {
+    alert('ユーザー検索に失敗しました');
+  })
+  });
+});

ab3725ea5ff5b0d3eeed6828abdbc3179252ae90
queq1890 <yujimatsumoto@matsumotohiroshinoMacBook-Pro.local>
2017-10-19 14:54:22 +0900

    Merge create.js into message.js
    create.jsとmessage.jsで共通の変数の記述があったので、同一のjsファイルに融合させた。

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,20 +3,49 @@ $(document).on('turbolinks:load',function() {
     if (message.image !== "") {
       var addImage = `<img src="${message.image}">`
      }
-    var html = `<div class="message" data-id="${message.id}">
-                  <div class="message__user">
-                    ${message.user_name}
-                  </div>
-                  <div class="message__date">
-                    ${message.created_at}
-                  </div>
-                  <div class="message__content">
-                    ${addImage} </br>
-                    ${message.body}
-                  </div>
-                </div>`
+    var html =`<div class="message" data-id="${message.id}">
+                <div class="message__user">
+                  ${message.user_name}
+                </div>
+                <div class="message__date">
+                  ${message.created_at}
+                </div>
+                <div class="message__content">
+                  ${addImage} </br>
+                  ${message.body}
+                </div>
+              </div>`
     return html;
   }
+  $('#chat_form').on('submit', function(e){
+    e.preventDefault();
+    var fd = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      type: 'POST',
+      url: url,
+      data: fd,
+      processData: false,
+      contentType: false,
+      dataType: 'json'
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      var scroll_target = $('.right_contents__messages')
+
+      $('.right_contents__messages').append(html)
+      $('.right_contents__input__text').val('');
+      $('.right_contents__input__file').val('');
+      scroll_target.animate({
+      scrollTop: scroll_target[0].scrollHeight
+    })
+      $('.right_contents__input__submit').prop('disabled', false);
+    })
+    .fail(function(){
+      alert('error');
+    });
+  });
+
   var interval = setInterval(function(){
     if (window.location.href.match(/\/groups\/\d+\/messages/)) {
     var message_id = $('.message:last').data('id');
